a43a9a8c82fe3a0f5b6e3ea26af42ca054e4e11d
Thiago Carvalheiro <carvalheiro.dev@gmail.com>
2023-05-26 22:00:33 -0300

    tipos de dados

---
+++ 20230526-INTRODUCAO-JAVASCRIPT/qualquernome.js
@@ -0,0 +1,37 @@
+"use strict";
+var numero;
+numero = 1;
+console.log(numero);
+var numero2;
+numero2 = 55;
+console.log(numero2);
+console.log(555555);
+
+var texto1;
+texto1 = "texto1";
+console.log(texto1);
+
+var texto2;
+texto2 = "lore ipsum fodsfhap afdoish paiods fh";
+console.log(texto2);
+console.log("lorem20 ipsum20");
+
+var boleano1;
+boleano1 = true;
+console.log(boleano1);
+
+var boleano2;
+boleano2 = false;
+console.log(boleano2);
+
+console.log(true);
+
+var nulo = null;
+console.log(nulo);
+
+var indefinido = undefined;
+console.log(indefinido);
+
+var indefinido2;
+
+console.log(indefinido2);
