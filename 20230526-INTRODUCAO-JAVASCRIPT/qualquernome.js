d95e909f76de8eecaf99fa53f6d6b8b5b7dffcfd
Thiago Carvalheiro <carvalheiro.dev@gmail.com>
2023-05-30 16:27:58 -0700

    Operadores aritméticos

--- 20230526-INTRODUCAO-JAVASCRIPT/qualquernome.js
+++ 20230526-INTRODUCAO-JAVASCRIPT/qualquernome.js
@@ -1,37 +1,13 @@
-"use strict";
-var numero;
-numero = 1;
-console.log(numero);
-var numero2;
-numero2 = 55;
-console.log(numero2);
-console.log(555555);
+var teste;
+teste = 1;
+console.log(teste);
 
-var texto1;
-texto1 = "texto1";
-console.log(texto1);
+var replica;
+replica = teste;
+console.log(replica);
 
-var texto2;
-texto2 = "lore ipsum fodsfhap afdoish paiods fh";
-console.log(texto2);
-console.log("lorem20 ipsum20");
+console.log("mudei o valor da gaveta teste");
+teste = "novo valor";
 
-var boleano1;
-boleano1 = true;
-console.log(boleano1);
-
-var boleano2;
-boleano2 = false;
-console.log(boleano2);
-
-console.log(true);
-
-var nulo = null;
-console.log(nulo);
-
-var indefinido = undefined;
-console.log(indefinido);
-
-var indefinido2;
-
-console.log(indefinido2);
+console.log(teste);
+console.log(replica);
---
+++ 20230530-EXPRESSOES-E-STATEMENTS-CONDICIONAIS/script.js
@@ -0,0 +1,75 @@
+var numero1;
+numero1 = 1;
+console.log(numero1 + 3); // 4
+numero1 = 5;
+console.log(numero1 + 5); // 10
+console.log('OPERADORES ARITMÉTICOS');
+numero1 = numero1 + numero1;
+console.log(numero1); // 10
+console.log("concatenando numero");
+console.log("1" + "1 teste"); // 11 Teste
+console.log(5 - 4); // 1
+console.log(5 * 4); // 20
+console.log(5 / 4); // 1.25
+console.log(4 - 3 * 5);
+console.log(4 - 3 * 5 / 2);
+// console.log(4 - 15 / 2)
+// console.log(4 - 7.5)
+// console.log(-3.5)  
+console.log((4 - 3 * 5) * 5 / 2)
+// console.log(1 * 5 / 2)
+// console.log(5 / 2)
+// console.log(2.5)
+
+
+console.log('OPERADORES DE COMPARAÇÃO');
+
+console.log(false === false) // true
+console.log(true === false) // false
+console.log(1 === 3) // false
+console.log(1 === 1) // true
+console.log('1' === 1) 
+
+console.log(1 == 1) // true
+console.log('1' == 1) // true
+console.log(0 == false) //
+
+console.log(1 !== false)
+console.log(1 !== 1) // false
+console.log(true !== true) // false
+console.log('1' !== 1) // true
+
+console.log(1 != 1) // false
+console.log('1' != 1) // false
+
+console.log(1 > 1) // false
+console.log(2 > 1) // true
+console.log(3 > 4) // false
+
+console.log(1 < 2) // true
+console.log(2 < 1) // false
+
+console.log(1 >= 1) // true
+console.log(1 >= 2) // false
+
+console.log(1 <= 1) // true
+console.log(2 <= 1) // fa;se
+
+// OPERADORES LÓGICOS
+console.log('OPERADORES LÓGICOS');
+console.log(true && true) // true
+console.log(true && false) // false
+console.log(false && false) // false
+
+console.log(true || true) // true
+console.log(true || false) // true
+console.log(false || false) // false
+
+var condicao1 = 5 === 5; // true
+var condicao2 = 5 > 6; // false
+
+console.log(condicao1 && condicao2) // false
+console.log(condicao1 || condicao2) // true
+
+console.log('combinação de operadores')
+console.log(5 === 5 && 20 > 19)
