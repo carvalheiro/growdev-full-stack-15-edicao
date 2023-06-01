66ea7b3fa4b70b0d617db46ebba8b81a05f9fddf
Thiago Carvalheiro <carvalheiro.dev@gmail.com>
2023-06-01 15:20:23 -0700

    aula

---
+++ 20230530-EXPRESSOES-E-STATEMENTS-CONDICIONAIS/exercicio-2.js
@@ -0,0 +1,7 @@
+var nome = "Thiago";
+
+var idadeEmAnos = nome;
+var idadeEmMeses = idadeEmAnos * 12;
+var idadeEmDias = idadeEmAnos * 365;
+
+console.log("A idade de " + nome + " em anos é " + idadeEmDias);
