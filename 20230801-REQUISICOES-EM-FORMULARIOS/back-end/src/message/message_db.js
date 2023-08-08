55c30abce3c043be50ceb9512b75e7b375355546
Thiago Carvalheiro <carvalheiro.dev@gmail.com>
2023-08-07 17:35:34 -0700

    revisao

--- 20230801-REQUISICOES-EM-FORMULARIOS/back-end/src/message/message_db.js
+++ 20230801-REQUISICOES-EM-FORMULARIOS/back-end/src/message/message_db.js
@@ -4,68 +4,101 @@ import UserDB from "../user/user_db.js";
 let messagesList = [];
 let nextID = 1;
 
-export default class MessageDB
-{
-    //returns the new message
-    //throws an error if user does not exist
-    static add(userId, title, description)
-    {
-        UserDB.find(userId);
-        
-        let newMessage = new Message(nextID++, userId, title, description);
-        messagesList.push(newMessage);
-        return newMessage;    
+export default class MessageDB {
+  //returns the new message
+  //throws an error if user does not exist
+  static add(userId, title, description) {
+    UserDB.find(userId);
+
+    let newMessage = new Message(nextID++, userId, title, description);
+    messagesList.push(newMessage);
+    return newMessage;
+  }
+
+  //returns the removed message
+  //throws an error if message does not exist
+  static remove(id) {
+    let index = messagesList.findIndex((message) => message.id === id);
+    if (index === -1) {
+      throw new Error("Message with id '" + id + "' does not exist");
     }
 
-    //returns the removed message
-    //throws an error if message does not exist
-    static remove(id) 
-    {
-        let index = messagesList.findIndex((message) => message.id === id);
-        if (index === -1)
-        {
-            throw new Error("Message with id '" + id + "' does not exist");
-        }
+    let deletedMessages = messagesList.splice(index, 1);
+    return deletedMessages[0];
+  }
 
-        let deletedMessages = messagesList.splice(index, 1);
-        return deletedMessages[0];    
+  //finds a message by ID
+  //throws an error if message does not exist
+  static find(id) {
+    let message = messagesList.find((message) => message.id === id);
+    if (!message) {
+      throw new Error("Message with id '" + id + "' does not exist");
     }
+    return message;
+  }
 
-    //finds a message by ID
-    //throws an error if message does not exist
-    static find(id)
-    {
-        let message = messagesList.find((message) => message.id === id);
-        if (!message)
-        {
-            throw new Error("Message with id '" + id + "' does not exist");
-        }
-        return message;    
-    }
+  //finds all user's messages
+  //returns an array
+  static findAll(userId, pagina) {
+    console.log(pagina);
+    const itemsPerPage = 3;
+    // pagina 1 -> indice 0 -> indice final 2 -> diferenca entre final e inicio = 2
+    // pagina 2 -> indice 3 -> indice final 5 -> diferenca entre final e inicio = 2
+    // pagina 3 -> indice 6 -> indice final 8 -> diferenca entre final e inicio = 2
 
-    //finds all user's messages
-    //returns an array
-    static findAll(userId)
-    {
-        let messages = messagesList.filter((message) => message.userId === userId);
-        return messages;
-    }
+    // 4 items por pagina
+    // pagina 1 -> indice 0 -> indice final 3 -> diferenca entre final e inicio = 3
+    // pagina 2 -> indice 4 -> indice final 7 -> diferenca entre final e inicio = 3
+    // pagina 3 -> indice 8 -> indice final 11 -> diferenca entre final e inicio = 3
 
-    //Updates a message data
-    //Undefined properties are not updated
-    static update(id, title, description)
-    {
-        let message = this.find(id);
+    // 5 items por pagina
+    // pagina 1 -> indice 0 -> indice final 4 -> diferenca entre final e inicio = 4
+    // pagina 2 -> indice 5 -> indice final 9 -> diferenca entre final e inicio = 4
+    // pagina 3 -> indice 10 -> indice final 14 -> diferenca entre final e inicio = 4
 
-        if (title !== undefined)
-            message.title = title;
-     
-        if (description !== undefined)
-            message.description = description;
-     
-        return message;
-    }
+    const indiceInicialDaNossaPagina = (pagina - 1) * itemsPerPage;
+    const indiceFinalDaNossaPagina =
+      indiceInicialDaNossaPagina + itemsPerPage - 1;
+    let messagensTotais = messagesList.filter(
+      (message) => message.userId === userId
+    );
+    let messages = [];
+    messagensTotais.forEach(function (message, indice) {
+      if (
+        indice >= indiceInicialDaNossaPagina &&
+        indice <= indiceFinalDaNossaPagina
+      ) {
+        messages.push(message);
+      }
+    });
+    return messages;
+  }
+
+  //Updates a message data
+  //Undefined properties are not updated
+  static update(id, title, description) {
+    let message = this.find(id);
+
+    if (title !== undefined) message.title = title;
+
+    if (description !== undefined) message.description = description;
+
+    return message;
+  }
 }
 
 //fill DB with some data
-MessageDB.add(1, "Wellcome John Doe", "We wellcome you with this first message");
+MessageDB.add(
+  1,
+  "Wellcome John Doe",
+  "We wellcome you with this first message"
+);
+MessageDB.add(1, "Second message", "This is the second message");
+MessageDB.add(1, "Third message", "This is the third message");
+MessageDB.add(1, "Fourth message", "This is the fourth message");
+MessageDB.add(1, "Fifth message", "This is the fifth message");
+MessageDB.add(1, "Sixth message", "This is the sixth message");
+MessageDB.add(1, "Seventh message", "This is the seventh message");
+MessageDB.add(1, "Eighth message", "This is the eighth message");
+MessageDB.add(1, "Ninth message", "This is the ninth message");
+MessageDB.add(1, "Tenth message", "This is the tenth message");
--- 20230801-REQUISICOES-EM-FORMULARIOS/back-end/src/message/message_routes.js
+++ 20230801-REQUISICOES-EM-FORMULARIOS/back-end/src/message/message_routes.js
@@ -3,17 +3,13 @@ import { Router } from "express";
 
 export const messageRouter = Router();
 
-function executeAndRespond(responseObj, func)
-{
-    try
-    {
-        let result = func();
-        responseObj.status(200).send(result);
-    }
-    catch(error)
-    {
-        responseObj.status(400).send(error.message);
-    }
+function executeAndRespond(responseObj, func) {
+  try {
+    let result = func();
+    responseObj.status(200).send(result);
+  } catch (error) {
+    responseObj.status(400).send(error.message);
+  }
 }
 
 //--
@@ -21,31 +17,43 @@ function executeAndRespond(responseObj, func)
 //--
 
 //Create
-messageRouter.post('/message', function (req, res) 
-{
-    executeAndRespond(res, () => MessageDB.add(parseInt(req.body.userId), req.body.title, req.body.description));
+messageRouter.post("/message", function (req, res) {
+  executeAndRespond(res, () =>
+    MessageDB.add(
+      parseInt(req.body.userId),
+      req.body.title,
+      req.body.description
+    )
+  );
 });
 
 //Read specific message
-messageRouter.get('/message/:id', function (req, res) 
-{
-    executeAndRespond(res, () => MessageDB.find(parseInt(req.params.id)));
+messageRouter.get("/message/:id", function (req, res) {
+  executeAndRespond(res, () => MessageDB.find(parseInt(req.params.id)));
 });
 
 //Read all user messages
-messageRouter.get('/user_messages/:userId', function (req, res) 
-{
-    executeAndRespond(res, () => MessageDB.findAll(parseInt(req.params.userId)));
+messageRouter.get("/user_messages/:userId", function (req, res) {
+  executeAndRespond(res, () => {
+    return MessageDB.findAll(
+      parseInt(req.params.userId),
+      parseInt(req.query.pagina)
+    );
+  });
 });
 
 //Update
-messageRouter.put('/message/:id', function (req, res)
-{
-    executeAndRespond(res, () => MessageDB.update(parseInt(req.params.id), req.body.title, req.body.description));
+messageRouter.put("/message/:id", function (req, res) {
+  executeAndRespond(res, () =>
+    MessageDB.update(
+      parseInt(req.params.id),
+      req.body.title,
+      req.body.description
+    )
+  );
 });
 
 //Delete
-messageRouter.delete('/message/:id', function (req, res) 
-{
-    executeAndRespond(res, () => MessageDB.remove(parseInt(req.params.id)));
+messageRouter.delete("/message/:id", function (req, res) {
+  executeAndRespond(res, () => MessageDB.remove(parseInt(req.params.id)));
 });
